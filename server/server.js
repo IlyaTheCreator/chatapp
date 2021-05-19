f4650f6cae6eb8a22f73e7d255f773c8836675ee
ilyaTheCreator <respondatanytime@gmail.com>
2021-05-19 16:39:11 +0300

    event listeners

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require("path")
+const http = require("http")
 const express = require("express")
+const socketIO = require("socket.io")
 
 const publicPath = path.join(__dirname, "/../public")
 const PORT = process.env.PORT || 3000;
 const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
+
+io.on("connection", socket => {
+    console.log("A new user just connected");
+
+    socket.on("disconnect", () => {
+        console.log("User was disconnected");
+    })
+})
 
 app.use(express.static(publicPath))
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
