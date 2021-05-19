1f78bdb0d6e7732609e37126a624876c536a0e7b
ilyaTheCreator <respondatanytime@gmail.com>
2021-05-19 20:47:17 +0300

    commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,175 @@
 const socket = io()
+let myNumber = 0
+let isAdmin = false
 
-socket.on("connect", function () {
-    console.log("Connected to server")
+const title = document.getElementById("title")
+const content = document.getElementById("content")
 
-    // socket.emit("createMessage", {
-    //     from: "iluha",
-    //     text: "hello, I'm iluha!"
-    // })
+function displayAdminPageLogin() {
+    isAdmin = true
+    title.innerText = "Admin's page"
+    content.innerHTML = ""
+
+    const wrapper = document.createElement("div")
+    wrapper.classList.add("admin-form-wrapper")
+
+    const login = document.createElement("input")
+    login.type = "text"
+    login.placeholder = "Логин"
+
+    const password = document.createElement("input")
+    password.type = "password"
+    password.placeholder = "Пароль"
+
+    const btn = document.createElement("button")
+    btn.innerText = "Войти"
+
+    btn.addEventListener("click", () => {
+        socket.emit("signinAdmin", {
+            login: login.value,
+            password: password.value
+        })
+    })
+
+    wrapper.appendChild(login)
+    wrapper.appendChild(password)
+    wrapper.appendChild(btn)
+
+    content.appendChild(wrapper)
+}
+
+function selectWhoYouAre() {
+    title.innerText = "Выбери, кто ты:"
+
+    const btn1 = document.createElement("button")
+    const btn2 = document.createElement("button")
+
+    btn1.innerText = "админ"
+    btn2.innerText = "шрек"
+
+    btn1.addEventListener("click", () => {
+        socket.emit("adminEntered")
+    })
+
+    btn2.addEventListener("click", () => {
+        socket.emit("userEntered")
+    })
+
+    content.appendChild(btn1)
+    content.appendChild(btn2)
+}
+
+socket.on("connect", function() {
+    selectWhoYouAre()
+
+    socket.on("disableAdminBtn", function() {
+        // it disables the button somehow
+        console.log(content.childNodes[1])
+    })
+
+    socket.on("userPage", function(data) {
+        myNumber = data.users
+        title.innerText = "User's page"
+
+        content.innerHTML = ""
+
+        socket.on("wait", function(data) {
+            title.innerText = `Собрано ${data.numberOfUsers} из ${data.requiredNumberOfUsers} пользователей`
+            content.innerHTML = ""
+        })
+    })
+
+    socket.on("adminPage", function() {
+        displayAdminPageLogin()
+
+        socket.on("showAdminPage", function() {
+            console.log("empty")
+            content.innerHTML = ""
+
+            const input = document.createElement("input")
+            input.type = "text"
+            input.placeholder = "Количество пользователей"
+
+            const btn = document.createElement("button")
+            btn.innerText = "задать"
+
+            btn.addEventListener("click", () => {
+                socket.emit("setNumberOfUsers", parseInt(input.value))
+                input.value = ""
+            })
+
+            content.innerHTML = ""
+            content.appendChild(input)
+            content.appendChild(btn)
+        })
+
+        socket.on("wrongAdmin", function() {
+            displayAdminPageLogin()
+            title.innerText = "Неверный логин/пароль"
+        })
+    })
 })
 
-socket.on("disconnect", function () {
-    console.log("Disconnected from server");
+socket.on("getOut", function() {
+    title.innerText = "Пошел отсюда! (так надо)"
+
+    content.innerHTML = ""
 })
 
-socket.on("newMessage", function (message) {
-    console.log(message)
+socket.on("run", function(data) {
+    // if (data == myNumber) {
+        console.log(data, myNumber)
+
+        const header = document.createElement("h1")
+
+        header.innerText = "me!"
+        header.id = "loop-header"
+
+        document.querySelector("body").appendChild(header)
+
+        setTimeout(() => {
+            document.getElementById("loop-header").remove()
+        }, 1000)
+    // }
 })
+
+socket.on("showStopRunBtn", function(data) {
+    if (isAdmin) {
+        const btn = document.createElement("button")
+        btn.innerText = "стоп"
+
+        btn.addEventListener("click", () => {
+            socket.emit("stopTheRun")
+        })
+
+        content.appendChild(btn)
+    }
+})
+
+socket.on("seq-num", function(data) {
+    console.log(data)
+})
+
+// socket.on("connect", function() {
+//     console.log("Connected to server")
+
+//     // socket.emit("createMessage", {
+//     //     from: "iluha",
+//     //     text: "hello, I'm iluha!"
+//     // })
+// })
+
+// socket.on("disconnect", function() {
+//     console.log("Disconnected from server");
+// })
+
+// socket.on("newMessage", function(message) {
+//     console.log(message)
+// })
+
+// socket.emit("createMessage", {
+//     from: "Iluha",
+//     text: "Hello from iluha!"
+// }, function(message) {
+//     console.log(message + ". Server's got it.")
+// })
--- server/server.js
+++ server/server.js
@@ -1,57 +1,130 @@
-const path = require("path")
-const http = require("http")
-const express = require("express")
-const socketIO = require("socket.io")
+import path from "path"
+import http from "http"
+import express from "express"
+import { Server } from "socket.io"
 
-const publicPath = path.join(__dirname, "/../public")
+
+const __dirname = path.resolve(path.dirname(''));
+console.log(__dirname)
+const publicPath = path.join(__dirname, "/public")
 const PORT = process.env.PORT || 3000;
 const app = express()
 const server = http.createServer(app)
-const io = socketIO(server)
+const io = new Server(server)
+
+let numberOfUsers = 0
+let requiredNumberOfUsers = 0
+let isAdminHere = false
+let runGoing = true
 
 io.on("connection", socket => {
-    console.log("A new user just connected");
 
-    socket.emit("newMessage", {
-        from: "Admin",
-        text: "Welcome to the chat app!",
-        createdAt: new Date().getTime()
+    let sequenceNumberByClient = new Map();
+    
+    const startTheRun = () => {
+        if (runGoing) {
+            for (let i = 1; i <= requiredNumberOfUsers; i++) {
+                setTimeout(() => {
+                    io.sockets.to(i).emit("run")
+                    // io.emit("run", { id: i})
+        
+                    if (i == requiredNumberOfUsers) {
+                        startTheRun()
+                    }
+                }, 1000)
+            }
+        }
+    }
+
+    socket.on("userEntered", () => {
+        numberOfUsers += 1
+
+        if (numberOfUsers <= requiredNumberOfUsers) {
+            socket.emit("userPage", {users: numberOfUsers})
+            console.log("user just got in")
+
+            if (numberOfUsers < requiredNumberOfUsers) {
+                io.emit("wait", {
+                    numberOfUsers: numberOfUsers,
+                    requiredNumberOfUsers: requiredNumberOfUsers
+                })
+            } else {
+                console.log("let's go!")
+                io.emit("showStopRunBtn")
+
+                startTheRun()
+            }
+        } else {
+            socket.emit("getOut")
+            console.log("get out")
+        }
     })
 
-    socket.broadcast.emit("newMessage", {
-        from: "Admin",
-        text: "New user joined",
-        createdAt: new Date().getTime()
+    socket.on("adminEntered", () => {
+        if (isAdminHere) {
+            socket.emit("disableAdminBtn")
+        } else {
+            socket.emit("adminPage")
+        }
     })
 
+    socket.on("signinAdmin", (data) => {
+        if (data.login == "111" && data.password == "111") {
+            isAdminHere = true
+            console.log("admin just got in")
 
-    // socket.emit("newMessage", {
-    //     from: "iluha",
-    //     text: "hello, I'm iluha!"
+            socket.emit("showAdminPage")
+
+            socket.on("setNumberOfUsers", function(users) {
+                requiredNumberOfUsers = users
+                console.log(`Будет ${requiredNumberOfUsers} пользователей`)
+            })
+        } else {
+            socket.emit("wrongAdmin")
+        }
+    })
+
+    socket.on("stopTheRun", function() {
+        console.log("stopped the loop")
+        runGoing = false
+    })
+
+    setInterval(() => {
+        for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
+            client.emit("seq-num", sequenceNumber);
+            sequenceNumberByClient.set(client, sequenceNumber + 1);
+        }
+    }, 1000);
+
+    // console.log("A new user just connected");
+
+    // socket.emit("newMessage", generateMessage("Admin", "Welcome to the chat app!"))
+    // socket.broadcast.emit("newMessage",  generateMessage("Admin", "New user joined"))
+
+
+    // // socket.emit("newMessage", {
+    // //     from: "iluha",
+    // //     text: "hello, I'm iluha!"
+    // // })
+
+    // socket.on("createMessage", (message, callback) => {
+    //     console.log(`createMessage: ${message.text}`);
+
+    //     // broadcasting:
+    //     io.emit("newMessage", generateMessage(message.from, message.text))
+    //     // broadcast to all other people and not for me
+    //     // socket.broadcast.emit("newMessage", generateMessage(message.from, message.text))
+
+    //     callback("this is the server") // this runs in the browser 
+    //     // the callback from socket.emit() // 3rd argument
     // })
 
-    socket.on("createMessage", message => {
-        console.log(`createMessage: ${message.text}`);
-
-        // broadcasting:
-        // io.emit("newMessage", {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // })
-
-        // broadcast to all other people and not for me
-        socket.broadcast.emit("newMessage", {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
-    })
-
-    socket.on("disconnect", () => {
-        console.log("User was disconnected");
-    })
+    // socket.on("disconnect", () => {
+    //     console.log("User was disconnected");
+    // })
 })
+
+
 
 app.use(express.static(publicPath))
 
